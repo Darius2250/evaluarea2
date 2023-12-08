671aabbdd8409200833f0682535737d2e96560a9
Darius2250 <148950813+Darius2250@users.noreply.github.com>
2023-12-08 22:48:47 +0200

    Add files via upload

---
+++ Mihut_Darius.js
@@ -0,0 +1,25 @@
+<script>
+    let currentIndex = 0;
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.style.display = i === index ? 'block' : 'none';
+        });
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % totalSlides;
+        showSlide(currentIndex);
+    }
+
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+        showSlide(currentIndex);
+    }
+
+    setInterval(nextSlide, 3000);
+</script>
+
+	
